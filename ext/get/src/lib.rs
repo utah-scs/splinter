2926bcefb8e651b5b6252a10300127af7131ca70
Chinmay Kulkarni <chnm.kulkarni@gmail.com>
2018-03-14 18:23:03 -0600

    get() and put() extensions return a generator.
     The get() and put() extensions now return ownership of a Box<Generator>.
     This generator can then be run inside the database.

--- ext/get/src/lib.rs
+++ ext/get/src/lib.rs
@@ -15,8 +15,12 @@
 
 #![crate_type = "dylib"]
 #![feature(no_unsafe)]
+#![feature(generators, generator_trait)]
 
 extern crate sandstorm;
+
+use std::rc::Rc;
+use std::ops::Generator;
 
 use sandstorm::db::DB;
 
@@ -26,47 +30,65 @@ use sandstorm::db::DB;
 ///
 /// * `db`: An argument whose type implements the `DB` trait which can be used
 ///         to interact with the database.
+///
+/// # Return
+///
+/// A coroutine that can be run inside the database.
 #[no_mangle]
-pub fn init(db: &DB) {
-    // First off, retrieve the arguments to the extension.
-    let args = db.args();
+#[allow(unused_assignments)]
+pub fn init(db: Rc<DB>) -> Box<Generator<Yield=u64, Return=u64>> {
+    Box::new(move || {
+        let mut obj = None;
 
-    // Check that the arguments received is long enough to contain an 8 byte
-    // table id and a key to be looked up. If not, then write an error message
-    // to the response and return to the database.
-    if args.len() <= 8 {
-        let error = "Invalid args";
-        db.resp(error.as_bytes());
-        return;
-    }
+        {
+            // First off, retrieve the arguments to the extension.
+            let args = db.args();
 
-    // Next, split the arguments into a view over the table identifier (first
-    // eight bytes), and a view over the key to be looked up. De-serialize the
-    // table identifier into a u64.
-    let (table, key) = args.split_at(8);
-    let table: u64 = *table.get(0).unwrap() as u64 +
-                        (*table.get(1).unwrap() as u64) * 2^8 +
-                        (*table.get(2).unwrap() as u64) * 2^16 +
-                        (*table.get(3).unwrap() as u64) * 2^24 +
-                        (*table.get(4).unwrap() as u64) * 2^32 +
-                        (*table.get(5).unwrap() as u64) * 2^40 +
-                        (*table.get(6).unwrap() as u64) * 2^48 +
-                        (*table.get(7).unwrap() as u64) * 2^56;
+            // Check that the arguments received is long enough to contain an
+            // 8 byte table id and a key to be looked up. If not, then write
+            // an error message to the response and return to the database.
+            if args.len() <= 8 {
+                let error = "Invalid args";
+                db.resp(error.as_bytes());
+                return 1;
+            }
+
+            // Next, split the arguments into a view over the table identifier
+            // (first eight bytes), and a view over the key to be looked up.
+            // De-serialize the table identifier into a u64.
+            let (table, key) = args.split_at(8);
+            let table: u64 = *table.get(0).unwrap() as u64 +
+                                (*table.get(1).unwrap() as u64) * 2^8 +
+                                (*table.get(2).unwrap() as u64) * 2^16 +
+                                (*table.get(3).unwrap() as u64) * 2^24 +
+                                (*table.get(4).unwrap() as u64) * 2^32 +
+                                (*table.get(5).unwrap() as u64) * 2^40 +
+                                (*table.get(6).unwrap() as u64) * 2^48 +
+                                (*table.get(7).unwrap() as u64) * 2^56;
 
 
-    // Finally, lookup the database for the object, and populate the response.
-    match db.get(table, key) {
-        // If the object was found, write it to the response.
-        Some(val) => {
-            db.resp(val.read());
-            println!("Table: {}, Key: {:?}, Value: {:?}",
-                     table, key, val.read());
+            // Finally, lookup the database for the object.
+            obj = db.get(table, key);
         }
 
-        // If the object was not found, write an error message to the response.
-        None => {
-            let error = "Object does not exist";
-            db.resp(error.as_bytes());
+        // Yield down to the database after issuing the get().
+        yield 0;
+
+        // Populate a response to the tenant.
+        match obj {
+            // If the object was found, write it to the response.
+            Some(val) => {
+                db.resp(val.read());
+                return 0;
+            }
+
+            // If the object was not found, write an error message to the
+            // response.
+            None => {
+                let error = "Object does not exist";
+                db.resp(error.as_bytes());
+                return 0;
+            }
         }
-    }
+    })
 }
--- ext/put/src/lib.rs
+++ ext/put/src/lib.rs
@@ -15,8 +15,12 @@
 
 #![crate_type = "dylib"]
 #![feature(no_unsafe)]
+#![feature(generators, generator_trait)]
 
 extern crate sandstorm;
+
+use std::rc::Rc;
+use std::ops::Generator;
 
 use sandstorm::db::DB;
 
@@ -26,86 +30,112 @@ use sandstorm::db::DB;
 ///
 /// * `db`: An argument whose type implements the `DB` trait which can be used
 ///         to interact with the database.
+///
+/// # Return
+///
+/// A coroutine that can be run inside the database.
 #[no_mangle]
-pub fn init(db: &DB) {
-    // First off, retrieve the arguments to the extension.
-    let args = db.args();
+#[allow(unused_assignments)]
+pub fn init(db: Rc<DB>) -> Box<Generator<Yield=u64, Return=u64>> {
+    Box::new(move || {
+        let mut alloc = None;
+        let mut val_offset = 0;
 
-    // Check that the arguments received is long enough to contain an 8 byte
-    // table id. If not, then write an error message to the response and
-    // return to the database.
-    if args.len() < 8 {
-        let error = "Invalid args";
-        db.resp(error.as_bytes());
-        return;
-    }
+        {
+            // First off, retrieve the arguments to the extension.
+            let args = db.args();
 
-    // Next, split the arguments into a view over the table identifier (first
-    // eight bytes), and a view over the key and value to be written.
-    // De-serialize the table identifier into a u64.
-    let (table, rem) = args.split_at(8);
-    let table: u64 = *table.get(0).unwrap() as u64 +
-                        (*table.get(1).unwrap() as u64) * 2^8 +
-                        (*table.get(2).unwrap() as u64) * 2^16 +
-                        (*table.get(3).unwrap() as u64) * 2^24 +
-                        (*table.get(4).unwrap() as u64) * 2^32 +
-                        (*table.get(5).unwrap() as u64) * 2^40 +
-                        (*table.get(6).unwrap() as u64) * 2^48 +
-                        (*table.get(7).unwrap() as u64) * 2^56;
+            // Check that the arguments received is long enough to contain an 8
+            // byte table id. If not, then write an error message to the
+            // response and return to the database.
+            if args.len() < 8 {
+                let error = "Invalid args";
+                db.resp(error.as_bytes());
+                return 1;
+            }
 
-    // Check that the remaining arguments is long enough to contain a two byte
-    // key length. If not, then write an error message to the response and
-    // return to the database.
-    if rem.len() < 2 {
-        let error = "Invalid args";
-        db.resp(error.as_bytes());
-        return;
-    }
+            // Next, split the arguments into a view over the table identifier
+            // (first eight bytes), and a view over the key and value to be
+            // written. De-serialize the table identifier into a u64.
+            let (table, rem) = args.split_at(8);
+            let table: u64 = *table.get(0).unwrap() as u64 +
+                            (*table.get(1).unwrap() as u64) * 2^8 +
+                            (*table.get(2).unwrap() as u64) * 2^16 +
+                            (*table.get(3).unwrap() as u64) * 2^24 +
+                            (*table.get(4).unwrap() as u64) * 2^32 +
+                            (*table.get(5).unwrap() as u64) * 2^40 +
+                            (*table.get(6).unwrap() as u64) * 2^48 +
+                            (*table.get(7).unwrap() as u64) * 2^56;
 
-    // Next, split the remaining arguments into a view over the key length, and
-    // a view over the object to be written.
-    let (key_len, obj) = rem.split_at(2);
-    let key_len: u16 = (*key_len.get(0).unwrap() as u16) +
-                        (*key_len.get(1).unwrap() as u16) * 256;
+            // Check that the remaining arguments is long enough to contain a
+            // two byte key length. If not, then write an error message to the
+            // response and return to the database.
+            if rem.len() < 2 {
+                let error = "Invalid args";
+                db.resp(error.as_bytes());
+                return 1;
+            }
 
-    // Check that the remaining arguments is long enough to contain the key. If
-    // not, write an error message to the response and return to the database.
-    if obj.len() < key_len as usize {
-        let error = "Invalid args";
-        db.resp(error.as_bytes());
-        return;
-    }
+            // Next, split the remaining arguments into a view over the key
+            // length, and a view over the object to be written.
+            let (key_len, obj) = rem.split_at(2);
+            let key_len: u16 = (*key_len.get(0).unwrap() as u16) +
+                                (*key_len.get(1).unwrap() as u16) * 256;
 
-    // Next, split the remaining arguments into a view over the key and value
-    // to be written, and request an allocation from the database.
-    let (key, val) = obj.split_at(key_len as usize);
-    match db.alloc(table, key, val.len() as u64) {
-        // If the allocation was successfull, write the value into it, and
-        // invoke the put() interface.
-        Some(mut buf) => {
-            buf.write_slice(val);
+            // Check that the remaining arguments is long enough to contain the
+            // key. If not, write an error message to the response and return
+            // to the database.
+            if obj.len() < key_len as usize {
+                let error = "Invalid args";
+                db.resp(error.as_bytes());
+                return 1;
+            }
 
-            // Hand over the object to the database.
-            match db.put(buf) {
-                // Indicate success in the response.
-                true => {
-                    let error = "Success";
-                    db.resp(error.as_bytes());
-                }
+            // Save the offset of the value in the payload for use after the
+            // yield.
+            val_offset += 8 + 2 + key_len;
 
-                // If the hand over failed, write an error message to the
-                // response.
-                false => {
-                    let error = "put() failed";
-                    db.resp(error.as_bytes());
+            // Next, split the remaining arguments into a view over the key and
+            // value to be written, and request an allocation from the database.
+            let (key, val) = obj.split_at(key_len as usize);
+            alloc = db.alloc(table, key, val.len() as u64);
+        }
+
+        // Yield down to the database.
+        yield 0;
+
+        match alloc {
+            // If the allocation was successfull, write the value into it, and
+            // invoke the put() interface.
+            Some(mut buf) => {
+                let val = db.args().split_at(val_offset as usize).1;
+                buf.write_slice(val);
+
+                // Hand over the object to the database.
+                match db.put(buf) {
+                    // Indicate success in the response.
+                    true => {
+                        let error = "Success";
+                        db.resp(error.as_bytes());
+                        return 0;
+                    }
+
+                    // If the hand over failed, write an error message to the
+                    // response.
+                    false => {
+                        let error = "put() failed";
+                        db.resp(error.as_bytes());
+                        return 1;
+                    }
                 }
             }
-        }
 
-        // If the allocation failed, write an error message to the response.
-        None => {
-            let error = "Allocation failed";
-            db.resp(error.as_bytes());
+            // If the allocation failed, write an error message to the response.
+            None => {
+                let error = "Allocation failed";
+                db.resp(error.as_bytes());
+                return 1;
+            }
         }
-    }
+    })
 }
