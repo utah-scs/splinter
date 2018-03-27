7fd181c417c7f3beb59a409f38a410467f25732d
Ryan Stutsman <stutsman@cs.utah.edu>
2018-03-27 16:34:38 -0600

    Fixed YCSB to work with more generic closures.
    Validated Zipf distribution with further testing. Distribution seems to
    check out for YCSB 0.99 when testing with a handful of keys.

--- db/src/bin/client/ycsb.rs
+++ db/src/bin/client/ycsb.rs
@@ -24,9 +24,6 @@ use self::rand::{Rng, XorShiftRng};
 use self::rand::distributions::Sample;
 use self::zipf::ZipfDistribution;
 
-type GetFn = fn(&[u8]);
-type PutFn = fn(&[u8], &[u8]);
-
 // YCSB A, B, and C benchmark.
 // The benchmark is created and parameterized with `new()`. Many threads
 // share the same benchmark instance. Each thread can call `abc()` which
@@ -42,8 +39,6 @@ struct Ycsb {
     n_keys: usize,
     put_pct: usize,
     skew: f64,
-    get: GetFn,
-    put: PutFn,
     done: AtomicBool,
 }
 
@@ -57,19 +52,14 @@ impl Ycsb {
     //  - n_keys: Number of keys from which random keys are drawn.
     //  - put_pct: Number between 0 and 100 indicating percent of ops that are sets.
     //  - skew: Zipfian skew parameter. 0.99 is YCSB default.
-    //  - get: A function that fetches the data stored under a bytestring key of `key_len` bytes.
-    //  - set: A function that stores the data stored under a bytestring key of `key_len` bytes
-    //         with a bytestring value of `value_len` bytes.
     // # Return
     //  A new instance of YCSB that threads can call `abc()` on to run.
-    fn new(
+    pub fn new(
         key_len: usize,
         value_len: usize,
         n_keys: usize,
         put_pct: usize,
         skew: f64,
-        get: GetFn,
-        put: PutFn,
     ) -> Ycsb {
         Ycsb {
             key_len: key_len,
@@ -77,18 +67,24 @@ impl Ycsb {
             n_keys: n_keys,
             put_pct: put_pct,
             skew: skew,
-            get: get,
-            put: put,
             done: AtomicBool::new(false),
         }
     }
 
     // Run YCSB A, B, or C (depending on `new()` parameters).
     // The calling thread will not return until `done()` is called on this `Ycsb` instance.
+    //
+    // # Arguments
+    //  - get: A function that fetches the data stored under a bytestring key of `self.key_len` bytes.
+    //  - set: A function that stores the data stored under a bytestring key of `self.key_len` bytes
+    //         with a bytestring value of `self.value_len` bytes.
     // # Return
     //  A three tuple consisting of the duration that this thread ran the benchmark, the
     //  number of gets it performed, and the number of puts it performed.
-    fn abc(&self) -> (Duration, u32, u32) {
+    pub fn abc<G, P>(&self, get: G, put: P) -> (Duration, u32, u32)
+        where G: Fn(&[u8]),
+              P: Fn(&[u8], &[u8]),
+    {
         // TODO(stutsman) It's unclear why this won't work.
         // let rng = XorShiftRng::from_rng(rand::thread_rng()).expect("Couldn't create PRNG.");
         let mut rng = XorShiftRng::new_unseeded();
@@ -115,10 +111,10 @@ impl Ycsb {
             key_buf[..mem::size_of::<u32>()].copy_from_slice(kslice);
 
             if is_get {
-                (self.get)(key_buf.as_slice());
+                get(key_buf.as_slice());
                 n_gets += 1;
             } else {
-                (self.put)(key_buf.as_slice(), value_buf.as_slice());
+                put(key_buf.as_slice(), value_buf.as_slice());
                 n_puts += 1;
             }
         }
@@ -127,29 +123,31 @@ impl Ycsb {
     }
 
     // Break all threads running `abc()`.
-    fn stop(&self) {
+    pub fn stop(&self) {
         self.done.store(true, Ordering::Relaxed);
     }
 }
 
+#[cfg(test)]
 mod test {
     use std;
     use std::thread;
     use std::time::Duration;
-    use std::sync::Arc;
+    use std::sync::{Arc, Mutex};
+    use std::collections::HashMap;
 
     fn get(_key: &[u8]) {}
     fn put(_key: &[u8], _value: &[u8]) {}
 
     #[test]
     fn ycsb_abc_basic() {
-        let b = Arc::new(super::Ycsb::new(10, 100, 1000000, 5, 0.99, get, put));
+        let b = Arc::new(super::Ycsb::new(10, 100, 1000000, 5, 0.99));
 
         let n_threads = 32;
         let mut threads = Vec::with_capacity(n_threads);
 
         for _ in 0..n_threads { let b = b.clone();
-            threads.push(thread::spawn(move || b.abc()));
+            threads.push(thread::spawn(move || b.abc(get, put)));
         }
 
         thread::sleep(Duration::from_secs(2));
@@ -178,5 +176,87 @@ mod test {
             n_puts as f64 / secs,
             (n_gets + n_puts) as f64 / secs
         );
+    }
+
+    fn convert_key(key: &[u8]) -> u32 {
+        assert_eq!(4, key.len());
+        let mut k: u32 = 0;
+        for b in key {
+            k *= 256;
+            k += *b as u32;
+        }
+        k
+    }
+
+    #[test]
+    fn ycsb_abc_histogram() {
+        let hist = Arc::new(Mutex::new(HashMap::new()));
+
+        let n_keys = 20;
+        let b = Arc::new(super::Ycsb::new(4, 100, 20, 5, 0.99));
+
+        let n_threads = 32;
+        let mut threads = Vec::with_capacity(n_threads);
+
+        for _ in 0..n_threads {
+            let b = b.clone();
+            let hist = hist.clone();
+            threads.push(thread::spawn(move || b.abc(
+                |key| { // get
+                    let k = convert_key(key);
+                    let mut ht = hist.lock().unwrap();
+                    ht.entry(k).or_insert((0, 0)).0 += 1;
+                },
+                |key, _value| { // put
+                    let k = convert_key(key);
+                    let mut ht = hist.lock().unwrap();
+                    ht.entry(k).or_insert((0, 0)).1 += 1;
+                })));
+        }
+
+        thread::sleep(Duration::from_secs(2));
+
+        b.stop();
+
+        // Iterate across all threads. Return a tupule whose first member consists
+        // of the highest execution time across all threads, and whose second member
+        // is the sum of the number of iterations run on each benchmark thread.
+        // Dividing the second member by the first, will yeild the throughput.
+        let (duration, n_gets, n_puts) = threads
+            .into_iter()
+            .map(|t| t.join().expect("ERROR: Thread join failed."))
+            .fold(
+                (Duration::new(0, 0), 0, 0),
+                |(ldur, lgets, lputs), (rdur, rgets, rputs)| {
+                    (std::cmp::max(ldur, rdur), lgets + rgets, lputs + rputs)
+                },
+            );
+
+        let secs = duration.as_secs() as f64 + (duration.subsec_nanos() as f64 / 1e9);
+        println!(
+            "{} threads: {:.0} gets/s {:.0} puts/s {:.0} ops/s",
+            n_threads,
+            n_gets as f64 / secs,
+            n_puts as f64 / secs,
+            (n_gets + n_puts) as f64 / secs
+        );
+
+        let ht = hist.lock().unwrap();
+        let mut kvs : Vec<_> = ht.iter().collect();
+        kvs.sort();
+        let v : Vec<_> = kvs.iter().map(|&(k, v)| println!("Key {:?}: {:?} gets/puts", k, v)).collect();
+        println!("Unique key count: {}", v.len());
+        assert_eq!(n_keys, v.len());
+
+        let total : i64 = kvs.iter().map(|&(_, &(g, s))| (g + s) as i64).sum();
+
+        let mut sum = 0;
+        for &(k, v) in kvs.iter() {
+            let &(g, s) = v;
+            sum += g + s;
+            let percentile = sum as f64 / total as f64;
+            println!("Key {:?}: {:?} percentile", k, percentile);
+        }
+        // For 20 keys median key should be near 4th key, so this checks out.
     }
 }
