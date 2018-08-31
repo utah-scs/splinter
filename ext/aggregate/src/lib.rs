2671256d79e1b89f0fe03c72e40d2e147b6a298f
Chinmay Kulkarni <chnm.kulkarni@gmail.com>
2018-08-31 13:44:11 -0600

    Aggregate works across multiple records.

--- ext/aggregate/src/lib.rs
+++ ext/aggregate/src/lib.rs
@@ -20,14 +20,18 @@ extern crate sandstorm;
 
 use sandstorm::boxed::Box;
 use sandstorm::db::DB;
-use sandstorm::pack::{consume, pack};
+use sandstorm::pack::pack;
 use sandstorm::rc::Rc;
 use sandstorm::Generator;
+use sandstorm::size_of;
+use sandstorm::vec::*;
 
 /// Status codes for the response to the tenant.
 const SUCCESSFUL: u8 = 0x01;
 const INVALIDARG: u8 = 0x02;
 const INVALIDKEY: u8 = 0x03;
+
+const KEYLENGTH: usize = 30;
 
 /// This function serves as the entry to the aggregate extension.
 ///
@@ -60,41 +64,75 @@ pub fn init(db: Rc<DB>) -> Box<Generator<Yield = u64, Return = u64>> {
 ///         to interact with the database.
 #[inline(always)]
 fn dispatch(db: Rc<DB>) {
-    let mut err = INVALIDARG;
-
     // Unwrap args, lookup the db, perform the aggregate.
     let arg: &[u8] = db.args();
-    let (table, key) = arg.split_at(8);
-    let table: u64 = 0 | table[0] as u64 | (table[1] as u64) << 8 | (table[2] as u64) << 16
-        | (table[3] as u64) << 24 | (table[4] as u64) << 32
-        | (table[5] as u64) << 40 | (table[6] as u64) << 48
-        | (table[7] as u64) << 56;
+    let (t, key) = arg.split_at(size_of::<u64>());
 
-    err = INVALIDKEY;
-    let res = db.get(table, key).and_then(|val| {
-        err = SUCCESSFUL;
-        Some(aggregate(0, val.read()))
-    });
+    // Get the table id from the unwrapped arguments.
+    let mut table: u64 = 0;
+    for (idx, e) in t.iter().enumerate() {
+        table |= (*e as u64) << (idx << 3);
+    }
 
-    // Write out a response.
+    // Retrieve the list of keys to aggregate across.
+    let obj = db.get(table, key);
+
+    // Error code and response defined upfront so that results are written only
+    // at the end of this function.
+    let mut err = INVALIDARG;
+    let mut res = 0;
+
+    // Try performing the aggregate if the key list was successfully retrieved.
+    if let Some(val) = obj {
+        let r = aggregate(Rc::clone(&db), table, val.read());
+        err = r.0;
+        res = r.1;
+    }
+
+    // First write in the response code.
     db.resp(pack(&err));
 
-    if let Some(sum) = res {
-        db.resp(pack(&sum));
+    // If the invocation was successful, then write in the result of the aggregation too.
+    if err == SUCCESSFUL {
+        db.resp(pack(&res));
     }
 }
 
-/// Aggregates a sequence of bytes into an unsigned 64 bit integer.
+/// Aggregates a column across a list of records.
 ///
 /// # Arguments
 ///
-/// * `init`: The initial value to be used in the aggregation.
-/// * `vec`:  Slice with the sequence of bytes to aggregate. Read from the database.
+/// * `db`:    An argument whose type implements the `DB` trait which can be used
+///            to interact with the database.
+/// * `table`: Table the records belong to. Required to look them up from the db.
+/// * `key`:   List of keys to lookup and aggregate across.
+///
+/// # Return
+/// A tupule consisting of an error code and the result of the aggregation. This
+/// result is valid only if the error code is `SUCCESSFUL`.
 #[inline(always)]
-fn aggregate(mut init: u64, vec: &[u8]) -> u64 {
-    for e in vec {
-        init += (*e) as u64;
+fn aggregate(db: Rc<DB>, table: u64, keys: &[u8]) -> (u8, u64) {
+    let mut col = Vec::new();
+
+    // Lookup every 'KEYLENGTH' Byte key in the list, and save the value's first byte
+    // inside a vector. Return if a key with a different length is encountered or if
+    // a lookup fails.
+    for key in keys.chunks(KEYLENGTH) {
+        if key.len() != KEYLENGTH {
+            return (INVALIDKEY, 0);
+        }
+
+        let obj = db.get(table, key);
+
+        match obj {
+            Some(val) => col.push(val.read()[0]),
+
+            None => return (INVALIDKEY, 0),
+        }
     }
 
-    init
+    // Aggregate the saved column.
+    let aggr = col.iter().fold(0, |a, e| a + (*e as u64));
+
+    (SUCCESSFUL, aggr)
 }
