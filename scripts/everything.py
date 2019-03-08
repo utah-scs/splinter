43739ceb552aa5b261f8cd7b7fabb4a4538480f0
Jacob Barzee <jbarzee@qualtrics.com>
2019-03-08 12:17:44 -0700

    added better logging and a hostpool

--- scripts/everything.py
+++ scripts/everything.py
@@ -68,16 +68,24 @@ class Host(object):
         self.__name = name
         self.__id = id
         self.__hostName = hostName
+        self.__logger = logging.getLogger(self.__id)
+
+    def setupLogger(self, logDirName, level):
+        logFile = '{}/{}.log'.format(logDirName, self.__id)
+        handler = logging.FileHandler(logFile)
+        self.__logger.setLevel(level)
+        self.__logger.addHandler(handler)
+        self.__logger.info("Begin Logging!")
 
     def getPublicName(self):
         return self.__name + '.utah.cloudlab.us'
 
-    def dump(self, level):
-        logger.log(level, '\t\tName: ' + self.__name)
-        logger.log(level, '\t\tId: ' + self.__id)
-        logger.log(level, '\t\thostName: ' + self.__hostName)
+    def dump(self, level, logger):
+        self.__logger.log(level, '\t\tName: ' + self.__name)
+        self.__logger.log(level, '\t\tId: ' + self.__id)
+        self.__logger.log(level, '\t\thostName: ' + self.__hostName)
 
-    def ssh(self, logger, user, cmd):
+    def execute(self, user, cmd):
         cmd = 'ssh {0}@{1} {2}'.format(user, self.getPublicName(), cmd)
         process = subprocess.Popen(cmd,
                                    shell=True,
@@ -86,17 +94,58 @@ class Host(object):
                                    stderr=subprocess.PIPE,
                                    close_fds=True)
         pout, perr = process.communicate()
-        logProcess(logger, cmd, pout, perr)
+        logProcess(self.__logger, cmd, pout, perr)
 
         if process.returncode:
             raise RemoteSubprocessException(self, cmd, process.returncode)
 
+class HostPool(object):
+    def __init__(self):
+        self.__hosts = []
+
+    def setupLogger(self, logDirName, level):
+        for host in self.__hosts:
+            host.setupLogger(logDirName, level)
+
+    def dump(self, level, logger):
+        for _, host in enumerate(self.__hosts):
+            host.dump(level, logger)
+
+    def addHost(self, host):
+        self.__hosts.append(host)
+
+    def execute(self, user, cmd):
+        tpool = pool.ThreadPool(processes=len(self.__hosts))
+        async_results = []
+        for host in self.__hosts:
+            def wrapExecute(host, user, cmd):
+                try:
+                    host.execute(user, cmd)
+                except SubprocessException as e:
+                    return e
+
+            async_result = tpool.apply_async(
+                wrapExecute, (host, user, cmd))
+            async_results.append(async_result)
+
+        for async_result in async_results:
+            return_val = async_result.get()
+            if return_val == None:
+                continue
+            elif isinstance(return_val, SubprocessException):
+                raise return_val
+            elif isinstance(return_val, Exception):
+                raise return_val
+            else:
+                raise Exception(
+                    'Unrecognized return value from ssh: {0}'.format(str(return_val)))
+
 
 class Cluster(object):
-    def __init__(self, logger, user, server):
-        self.__logger = logger
+    def __init__(self, user, server):
+        self.__logger = logging.getLogger('cluster')
         self.__server = None
-        self.__clients = []
+        self.__clients = HostPool()
         self.__user = user
 
         cmd = 'ssh {0}@{1} /usr/bin/geni-get manifest'.format(
@@ -126,62 +175,81 @@ class Cluster(object):
             if id == 'server' and self.__server == None:
                 self.__server = Host(name, id, hostName)
             else:
-                self.__clients.append(Host(name, id, hostName))
+                self.__clients.addHost(Host(name, id, hostName))
         if server == None:
             raise Exception("No server found!")
+        self
+
+
+    def setupLogger(self, logDirName, level):
+        self.__clients.setupLogger(logDirName, level)
+        self.__server.setupLogger(logDirName, level)
+
+        logFile = '{}/cluster.log'.format(logDirName)
+        handler = logging.FileHandler(logFile)
+        self.__logger.setLevel(level)
+        self.__logger.addHandler(handler)
+        self.__logger.info("Begin Logging!")
 
     def dump(self, level):
-        self.__logger.log(level, '1 server:')
-        self.__server.dump(level)
-
-        self.__logger.log(level, '{} clients:'.format(len(self.__clients)))
-        for i, client in enumerate(self.__clients):
-            self.__logger.log(level, '\t{}:'.format(i))
-            client.dump(level)
+        self.__logger.log(level, 'server:')
+        self.__server.dump(level, self.__logger)
+        self.__logger.log(level, 'clients:')
+        self.__clients.dump(level, self.__logger)
 
     def checkAuth(self):
-        self.__server.ssh(self.__logger, '-o StrictHostKeyChecking=no {}'.format(self.__user), '')
-
-        for i, client in enumerate(self.__clients):
-            client.ssh(self.__logger, '-o StrictHostKeyChecking=no {}'.format(self.__user), '')
-
-    def __executeOnClients(self, cmd):
-        tpool = pool.ThreadPool(processes=len(self.__clients))
-        async_results = []
-        for client in self.__clients:
-            c = client
-            def wrapSSHFunc(client, user, cmd, logger):
-                try:
-                    client.ssh(logger, user, cmd)
-                except SubprocessException as e:
-                    return e
-
-            async_result = tpool.apply_async(
-                wrapSSHFunc, (client, self.__user, cmd, self.__logger))
-            async_results.append(async_result)
-
-        for async_result in async_results:
-            return_val = async_result.get()
-            if return_val == None:
-                continue
-            elif isinstance(return_val, SubprocessException):
-                raise return_val
-            elif isinstance(return_val, Exception):
-                raise return_val
-            else:
-                raise Exception(
-                    'Unrecognized return value from ssh: {0}'.format(str(return_val)))
+        # this flag adds the server to the known hosts file automatically
+        self.__server.execute('-o StrictHostKeyChecking=no {}'.format(self.__user), '')
+        self.__clients.execute('-o StrictHostKeyChecking=no {}'.format(self.__user), '')
 
     def __executeOnServer(self, cmd):
-        self.__server.ssh(self.__logger, self.__user, cmd)
+        self.__server.execute(self.__user, cmd)
+
+    def __executeOnClients(self, cmd):
+        self.__clients.execute(self.__user, cmd)
 
     def setup(self, branch):
+        # Setup clients and servers in parallel
+        tpool = pool.ThreadPool(processes=2)
+        serverRes = tpool.apply_async(self.__setupServer, (branch,))
+        clientsRes = tpool.apply_async(self.__setupClients, (branch,))
+
+        # Wait for both to complete
+        serverRes.get()
+        clientsRes.get()
+        self.__setupNIC()
+
+    def __setupServer(self, branch):
         try:
-            logger.info("Server setup started...")
+            self.__logger.info("Server setup started...")
             self.__executeOnServer('"git clone https://github.com/utah-scs/splinter.git"')
             self.__executeOnServer('"cd splinter; git checkout {0}"'.format(branch))
-            self.__executeOnServer('"cd splinter; ./scripts/setup.py --full > /dev/null 2>&1"')  # beware, dirty trick. nic_info is local
-            self.__executeOnServer('"cd splinter; cat nic_info" > nic_info')
+            self.__executeOnServer('"cd splinter; ./scripts/setup.py --full > /dev/null 2>&1"')
+            self.__logger.info('Server setup concluded.')
+
+        except Exception as e:
+            self.__logger.error('Server setup failed!')
+            self.__logger.error(str(e))
+            exit(1)
+    
+    def __setupClients(self, branch):
+        try:
+            self.__logger.info("Clients setup started...")
+            self.__executeOnClients('"git clone https://github.com/utah-scs/splinter.git"')
+            self.__executeOnClients('"cd splinter; git checkout {0}"'.format(branch))
+            self.__executeOnClients('"cd splinter; ./scripts/setup.py --full > /dev/null 2>&1"')
+
+            self.__logger.info('Clients setup concluded.')
+
+        except Exception as e:
+            self.__logger.error('Clients setup failed!')
+            self.__logger.error(str(e))
+            exit(1)
+
+    def __setupNIC(self):
+        try:
+            logger.info("NIC setup started...")
+            self.__executeOnServer('"cd splinter; cat nic_info" > nic_info')  # beware, dirty trick. nic_info is local
 
             pci = subprocess.check_output("awk '/^pci/ { print $2; }' < nic_info", shell=True).decode("UTF-8").rstrip()
             if not pci:
@@ -194,31 +262,28 @@ class Cluster(object):
             self.__executeOnServer('"cd splinter; cp db/server.toml-example db/server.toml; \
                                  sed -E -i \'s/01:02:03:04:05:06/' + mac + '/;\' db/server.toml; \
                                  sed -E -i \'s/0000:04:00.1/' + pci + '/;\' db/server.toml"')
-            self.__logger.info('Server setup concluded.')
-
-        except Exception as e:
-            self.__logger.error('Server setup failed!')
-            self.__logger.error(str(e))
-            exit(1)
-
-        try:
-            self.__logger.info("Clients setup started...")
-            self.__executeOnClients('"git clone https://github.com/utah-scs/splinter.git"')
-            self.__executeOnClients('"cd splinter; git checkout {0}"'.format(branch))
-            self.__executeOnClients('"cd splinter; ./scripts/setup.py --full > /dev/null 2>&1"')
-
             self.__executeOnClients('"cd splinter; \
                                  echo "server_mac: ' + mac + '" >> nic_info; \
                                  ./scripts/create-client-toml"')
 
-            self.__logger.info('Clients setup concluded.')
+            self.__logger.info('NIC setup concluded.')
 
         except Exception as e:
-            self.__logger.error('Clients setup failed!')
+            self.__logger.error('NIC setup failed!')
             self.__logger.error(str(e))
             exit(1)
 
-    def wipe(self,):
+    def wipe(self):
+        # Wipe clients and servers in parallel
+        tpool = pool.ThreadPool(processes=2)
+        serverRes = tpool.apply_async(self.__wipeServer, ())
+        clientsRes = tpool.apply_async(self.__wipeClients, ())
+
+        # Wait for both to complete
+        serverRes.get()
+        clientsRes.get()
+
+    def __wipeServer(self):
         try:
             self.__logger.info("Server wipe started...")
             self.__executeOnServer('"rm -rf splinter"')
@@ -229,6 +294,7 @@ class Cluster(object):
             self.__logger.error(str(e))
             exit(1)
 
+    def __wipeClients(self):
         try:
             self.__logger.info("Clients wipe started...")
             self.__executeOnClients('"rm -rf splinter"')
@@ -240,6 +306,16 @@ class Cluster(object):
             exit(1)
 
     def build(self, branch):
+        # Build clients and servers in parallel
+        tpool = pool.ThreadPool(processes=2)
+        serverRes = tpool.apply_async(self.__buildServer, (branch,))
+        clientsRes = tpool.apply_async(self.__buildClients, (branch,))
+
+        # Wait for both to complete
+        serverRes.get()
+        clientsRes.get()
+
+    def __buildServer(self, branch):
         try:
             self.__logger.info("Server build started...")
             self.__executeOnServer('"cd splinter; git checkout {0}"'.format(branch))
@@ -252,6 +328,7 @@ class Cluster(object):
             self.__logger.error(str(e))
             exit(1)
 
+    def __buildClients(self, branch):
         try:
             self.__logger.info("Clients build started...")
             self.__executeOnClients('"cd splinter; git checkout {0}"'.format(branch))
@@ -264,35 +341,21 @@ class Cluster(object):
             self.__logger.error(str(e))
             exit(1)
 
-    def startServer(self):
-        try:
-            self.__logger.info("Server start started...")
-            self.__executeOnServer('"cd splinter; sudo scripts/run-server"')
-            self.__logger.info("Server start concluded...")
+    def kill(self, extension):
+        # Kill clients and servers in parallel
+        tpool = pool.ThreadPool(processes=2)
+        serverRes = tpool.apply_async(self.__killServer, ())
+        clientsRes = tpool.apply_async(self.__killClients, (extension,))
 
-        except Exception as e:
-            self.__logger.error('Server start failed!')
-            self.__logger.error(str(e))
-            exit(1)
+        # Wait for both to complete
+        serverRes.get()
+        clientsRes.get()
 
-    def killServer(self):
+    def __killServer(self):
         # TODO @jmbarzee implement kill server
         raise NotImplementedError()
 
-    def runYCSB(self, rates):
-        # TODO @jmbarzee add flexibility to extensions
-        try:
-            for rate in rates:
-                self.__logger.info("run YSCB started...")
-                self.__executeOnClients('"cd splinter; sudo ./scripts/run-ycsb {0}"'.format(rate))
-                self.__logger.info("run YSCB concluded...")
-
-        except Exception as e:
-            self.__logger.error('run YSCB failed!')
-            self.__logger.error(str(e))
-            exit(1)
-
-    def killClients(self, ext):
+    def __killClients(self, ext):
         try:
             self.__logger.info("Clients kill started...")
             self.__executeOnServer('"sudo kill -9 `pidof {0}`"'.format(ext))
@@ -300,6 +363,30 @@ class Cluster(object):
 
         except Exception as e:
             self.__logger.error('Clients kill failed!')
+            self.__logger.error(str(e))
+            exit(1)
+
+    def startServer(self):
+        try:
+            self.__logger.info("Server start started...")
+            self.__executeOnServer('"cd splinter; sudo scripts/run-server &"')
+            self.__logger.info("Server start concluded...")
+
+        except Exception as e:
+            self.__logger.error('Server start failed!')
+            self.__logger.error(str(e))
+            exit(1)
+
+    def runYCSB(self, rates):
+        try:
+            self.__logger.info("run YSCB started...")
+            for rate in rates:
+                self.__logger.info("\tYSCB({0})".format(rate))
+                self.__executeOnClients('"cd splinter; sudo ./scripts/run-ycsb {0}"'.format(rate))
+            self.__logger.info("run YSCB concluded...")
+
+        except Exception as e:
+            self.__logger.error('run YSCB failed!')
             self.__logger.error(str(e))
             exit(1)
 
@@ -401,6 +488,18 @@ if __name__ == '__main__':
     logger = logging.getLogger('')
     logger.setLevel(args.verbose)
 
+    # Setup Cluster
+    try:
+        cluster = Cluster(args.user, args.server)
+        cluster.setupLogger(logCurrent, args.verbose)
+    except Exception as e:
+        logger.error("Could not establish cluster information!")
+        if cluster:
+            cluster.dump(logging.ERROR)
+        exit(1)
+    cluster.dump(logging.INFO)
+    cluster.checkAuth()
+
     # Setup symlink
     logLatest = "./logs/latest"
     try:
@@ -409,21 +508,10 @@ if __name__ == '__main__':
         pass  # directory already exists
     run("cd {0}; ln -sf ../{1}/* ./".format(logLatest, logDir), logger)
 
-    # Setup Cluster
-    try:
-        cluster = Cluster(logger, args.user, args.server)
-    except Exception as e:
-        logger.error("Could not establish cluster information!")
-        cluster.dump(logging.ERROR)
-        exit(1)
-    cluster.dump(logging.INFO)
-    cluster.checkAuth()
-
     branch = args.branch
     if branch == 'current':
         branch = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).decode("UTF-8").strip()
         
-
     if args.push:
         logger.info("Pushing local changes..")
         # TODO @jmbarzee check that there are no unstaged changes
@@ -445,12 +533,11 @@ if __name__ == '__main__':
 
             cluster.startServer()
             cluster.runYCSB(rates)
-            cluster.killServer()
+            #cluster.killServer()
 
     elif cmd == 'kill':
         # TODO @jmbarzee check for --extension
-        cluster.killClients(args.e)
-        cluster.killServer()
+        cluster.kill(args.extension)
 
     elif cmd == 'bench':
         raise NotImplementedError()
