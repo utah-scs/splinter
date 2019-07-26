a0f6ee7cc1009a40d343973c44a9a0726bd43801
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2019-07-25 20:59:37 -0600

    Make generic client code run for YCSB type of workload.

--- splinter/src/bin/client/client.rs
+++ splinter/src/bin/client/client.rs
@@ -26,16 +26,23 @@ extern crate zipf;
 mod setup;
 mod ycsbt;
 
+use std::cell::RefCell;
 use std::fmt::Display;
 use std::sync::Arc;
 
 use db::config;
+use db::cycles;
 use db::e2d2::allocators::*;
+use db::e2d2::common::EmptyMetadata;
+use db::e2d2::headers::UdpHeader;
 use db::e2d2::interface::*;
 use db::e2d2::scheduler::*;
 use db::log::*;
 use db::master::Master;
+use db::rpc::parse_rpc_opcode;
+use db::wireformat::*;
 
+use splinter::sched::TaskManager;
 use splinter::sendrecv::SendRecv;
 use splinter::*;
 
@@ -55,6 +62,52 @@ where
     sender: Arc<dispatch::Sender>,
     //
     sendrecv: C,
+
+    // If true, RPC requests corresponding to native get() and put() operations are sent out. If
+    // false, invoke() based RPC requests are sent out.
+    native: bool,
+
+    table_id: u64,
+
+    // The number of response packets to wait for before printing out statistics.
+    responses: u64,
+
+    // Time stamp in cycles at which measurement started. Required to calculate observed
+    // throughput of the Sandstorm server.
+    start: u64,
+
+    // The total number of responses received so far.
+    recvd: u64,
+
+    // Vector of sampled request latencies. Required to calculate distributions once all responses
+    // have been received.
+    latencies: Vec<u64>,
+
+    // If true, this receiver will make latency measurements.
+    master: bool,
+
+    // Time stamp in cycles at which measurement stopped.
+    stop: u64,
+
+    // Total number of requests to be sent out.
+    requests: u64,
+
+    // Number of requests that have been sent out so far.
+    sent: u64,
+
+    // To keep the mapping between sent and received packets. The client doesn't want to send
+    // more than 32(XXX) outstanding packets.
+    outstanding: u64,
+
+    // To keep a mapping between each packet and request parameters. This information will be used
+    // when the server pushes back the extension.
+    manager: RefCell<TaskManager>,
+
+    // The length of the key.
+    key_len: usize,
+
+    // The length of the record.
+    record_len: usize,
 }
 
 impl<T, C> Client<T, C>
@@ -69,24 +122,250 @@ where
         tx_port: CacheAligned<PortQueue>,
         dst_ports: u16,
         sendrecv: C,
+        master: bool,
+        masterservice: Arc<Master>,
     ) -> Client<T, C> {
+        let resps = 34 * 1000 * 1000;
         Client {
             receiver: dispatch::Receiver::new(rx_port),
             sender: Arc::new(dispatch::Sender::new(config, tx_port, dst_ports)),
             sendrecv: sendrecv,
+            native: !config.use_invoke,
+            table_id: 1,
+            responses: resps,
+            start: cycles::rdtsc(),
+            recvd: 0,
+            latencies: Vec::with_capacity(resps as usize),
+            master: master,
+            stop: 0,
+            requests: config.num_reqs as u64,
+            sent: 0,
+            outstanding: 0,
+            manager: RefCell::new(TaskManager::new(Arc::clone(&masterservice))),
+            key_len: config.key_len,
+            record_len: config.key_len + config.value_len + 9,
+        }
+    }
+
+    #[inline]
+    fn send_native(&mut self) {
+        let curr = cycles::rdtsc();
+        match self.sendrecv.next_optype() {
+            OpCode::SandstormGetRpc => {
+                let (tenant, key) = self.sendrecv.get_get_request();
+                self.sender.send_get(tenant, self.table_id, &key, curr);
+                self.outstanding += 1;
+                self.sent += 1;
+            }
+
+            OpCode::SandstormPutRpc => {
+                let (tenant, key, val) = self.sendrecv.get_put_request();
+                self.sender
+                    .send_put(tenant, self.table_id, &key, &val, curr);
+                self.outstanding += 1;
+                self.sent += 1;
+            }
+
+            OpCode::SandstormMultiGetRpc => {}
+
+            _ => {
+                info!("Invalid RPC request");
+            }
+        }
+    }
+
+    #[inline]
+    fn send_invoke(&mut self) {
+        let curr = cycles::rdtsc();
+        let (tenant, request) = self.sendrecv.get_invoke_request();
+        let name_length = 8;
+        self.sender.send_invoke(tenant, name_length, &request, curr);
+        self.manager.borrow_mut().create_task(
+            curr,
+            &request,
+            tenant,
+            name_length as usize,
+            Arc::clone(&self.sender),
+        );
+    }
+
+    #[inline]
+    fn recv_native(&mut self, curr: u64, packet: Packet<UdpHeader, EmptyMetadata>) {
+        //TODO: Make it generic for each type of client, maybe forward
+        // packet to client specific code for some processing.
+        match parse_rpc_opcode(&packet) {
+            OpCode::SandstormGetRpc => {
+                let p = packet.parse_header::<GetResponse>();
+                let timestamp = p.get_header().common_header.stamp;
+                self.latencies.push(curr - timestamp);
+                p.free_packet();
+            }
+            OpCode::SandstormPutRpc => {
+                let p = packet.parse_header::<PutResponse>();
+                let timestamp = p.get_header().common_header.stamp;
+                self.latencies.push(curr - timestamp);
+                p.free_packet();
+            }
+
+            _ => {
+                packet.free_packet();
+            }
+        }
+        self.recvd += 1;
+        self.outstanding -= 1;
+    }
+
+    #[inline]
+    fn recv_invoke(&mut self, curr: u64, packet: Packet<UdpHeader, EmptyMetadata>) {
+        match parse_rpc_opcode(&packet) {
+            OpCode::SandstormInvokeRpc => {
+                let p = packet.parse_header::<InvokeResponse>();
+                let timestamp = p.get_header().common_header.stamp;
+                match p.get_header().common_header.status {
+                    // If the status is StatusOk then add the stamp to the latencies and
+                    // free the packet.
+                    RpcStatus::StatusOk => {
+                        self.latencies.push(curr - timestamp);
+                        self.manager.borrow_mut().delete_task(timestamp);
+                        self.recvd += 1;
+                        self.outstanding -= 1;
+                    }
+
+                    // If the status is StatusPushback then compelete the task, add the
+                    // stamp to the latencies, and free the packet.
+                    RpcStatus::StatusPushback => {
+                        let records = p.get_payload();
+                        self.manager.borrow_mut().update_rwset(
+                            timestamp,
+                            records,
+                            self.record_len,
+                            self.key_len,
+                        );
+                        self.outstanding -= 1;
+                    }
+
+                    _ => {}
+                }
+                p.free_packet();
+            }
+
+            // The response corresponds to a get() or put() RPC.
+            // The opcode on the response identifies the RPC type.
+            OpCode::SandstormGetRpc => {
+                let p = packet.parse_header::<GetResponse>();
+                let timestamp = p.get_header().common_header.stamp;
+                self.manager.borrow_mut().update_rwset(
+                    timestamp,
+                    p.get_payload(),
+                    self.record_len,
+                    self.key_len,
+                );
+                p.free_packet();
+            }
+
+            _ => packet.free_packet(),
         }
     }
 
     fn send(&mut self) {
-        let (tenant, req) = self.sendrecv.get_get_request();
-        self.sender.send_get(tenant, 1, &req, 1);
+        if self.requests <= self.sent {
+            return;
+        }
+
+        while self.outstanding < 32 {
+            if self.native == true {
+                self.send_native();
+            } else {
+                self.send_invoke();
+            }
+        }
     }
 
     fn recv(&mut self) {
+        if self.stop > 0 {
+            return;
+        }
+
         if let Some(mut packets) = self.receiver.recv_res() {
+            let curr = cycles::rdtsc();
             while let Some(packet) = packets.pop() {
-                packet.free_packet();
+                match parse_rpc_opcode(&packet) {
+                    OpCode::SandstormCommitRpc => {
+                        let p = packet.parse_header::<CommitResponse>();
+                        let timestamp = p.get_header().common_header.stamp;
+                        match p.get_header().common_header.status {
+                            RpcStatus::StatusTxAbort => {
+                                info!("Abort");
+                            }
+
+                            RpcStatus::StatusOk => {
+                                self.latencies.push(curr - timestamp);
+                            }
+
+                            _ => {}
+                        }
+                        self.recvd += 1;
+                        p.free_packet();
+                        continue;
+                    }
+
+                    _ => {}
+                }
+
+                // Process the packet when it is not a commit response.
+                if self.native == true {
+                    self.recv_native(curr, packet);
+                } else {
+                    self.recv_invoke(curr, packet);
+                }
             }
+        }
+
+        // The moment all response packets have been received, set the value of the
+        // stop timestamp so that throughput can be estimated later.
+        if self.responses <= self.recvd {
+            self.stop = cycles::rdtsc();
+        }
+    }
+}
+
+// Implementation of the `Drop` trait on PushbackRecv.
+impl<T, C> Drop for Client<T, C>
+where
+    T: PacketTx + PacketRx + Display + Clone + 'static,
+    C: SendRecv,
+{
+    fn drop(&mut self) {
+        // Calculate & print the throughput for all client threads.
+        println!(
+            "Client Throughput {}",
+            self.recvd as f64 / cycles::to_seconds(self.stop - self.start)
+        );
+
+        if self.stop == 0 {
+            panic!("The client thread received only {} packets", self.recvd);
+        }
+
+        // Calculate & print median & tail latency only on the master thread.
+        if self.master {
+            self.latencies.sort();
+
+            let m;
+            let t = self.latencies[(self.latencies.len() * 99) / 100];
+            match self.latencies.len() % 2 {
+                0 => {
+                    let n = self.latencies.len();
+                    m = (self.latencies[n / 2] + self.latencies[(n / 2) + 1]) / 2;
+                }
+
+                _ => m = self.latencies[self.latencies.len() / 2],
+            }
+
+            println!(
+                ">>> {} {}",
+                cycles::to_seconds(m) * 1e9,
+                cycles::to_seconds(t) * 1e9
+            );
         }
     }
 }
@@ -100,6 +379,11 @@ where
     fn execute(&mut self) {
         self.send();
         self.recv();
+        //self.manager.borrow_mut().execute_task();
+        if self.stop > 0 {
+            unsafe { FINISHED = true }
+            return;
+        }
     }
 
     fn dependencies(&mut self) -> Vec<usize> {
@@ -111,9 +395,9 @@ fn setup_send_recv<S>(
     ports: Vec<CacheAligned<PortQueue>>,
     scheduler: &mut S,
     _core: i32,
-    _master: bool,
+    master: bool,
     config: &config::ClientConfig,
-    _masterservice: Arc<Master>,
+    masterservice: Arc<Master>,
 ) where
     S: Scheduler + Sized,
 {
@@ -128,7 +412,9 @@ fn setup_send_recv<S>(
         config,
         ports[0].clone(),
         config.server_udp_ports as u16,
-        ycsbt::YCSBT::new(),
+        ycsbt::YCSBT::new(config),
+        master,
+        masterservice,
     )) {
         Ok(_) => {
             info!(
@@ -217,7 +503,6 @@ fn main() {
             std::thread::sleep(std::time::Duration::from_secs(2));
         }
     }
-    std::thread::sleep(std::time::Duration::from_secs(100));
 
     // Stop the client.
     net_context.stop();
--- splinter/src/bin/client/ycsbt.rs
+++ splinter/src/bin/client/ycsbt.rs
@@ -1,23 +1,117 @@
+/* Copyright (c) 2019 University of Utah
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+extern crate db;
+extern crate rand;
 extern crate splinter;
+
+use std::mem;
+use std::mem::transmute;
+
+use db::config::ClientConfig;
+use db::wireformat::OpCode;
+
+use rand::distributions::Sample;
+use rand::{Rng, SeedableRng, XorShiftRng};
+use zipf::ZipfDistribution;
 
 use self::splinter::sendrecv::SendRecv;
 
-pub struct YCSBT {}
+pub struct YCSBT {
+    put_pct: usize,
+    rng: Box<Rng>,
+    key_rng: Box<ZipfDistribution>,
+    tenant_rng: Box<ZipfDistribution>,
+    key_buf: Vec<u8>,
+    value_buf: Vec<u8>,
+    invoke_buf: Vec<u8>,
+}
 
 impl YCSBT {
-    pub fn new() -> YCSBT {
-        YCSBT {}
+    pub fn new(config: &ClientConfig) -> YCSBT {
+        let seed: [u32; 4] = rand::random::<[u32; 4]>();
+
+        let mut key_buf: Vec<u8> = Vec::with_capacity(config.key_len);
+        key_buf.resize(config.key_len, 0);
+        let mut value_buf: Vec<u8> = Vec::with_capacity(config.value_len);
+        value_buf.resize(config.value_len, 0);
+
+        let payload_len = "ycsbt".as_bytes().len()
+            + mem::size_of::<u64>()
+            + mem::size_of::<u32>()
+            + mem::size_of::<u32>()
+            + config.key_len;
+        let mut invoke_buf: Vec<u8> = Vec::with_capacity(payload_len);
+        invoke_buf.extend_from_slice("ycsbt".as_bytes());
+        invoke_buf.extend_from_slice(&unsafe { transmute::<u64, [u8; 8]>(1u64.to_le()) });
+        invoke_buf.extend_from_slice(&unsafe { transmute::<u32, [u8; 4]>(1u32.to_le()) });
+        invoke_buf.extend_from_slice(&unsafe { transmute::<u32, [u8; 4]>(1u32.to_le()) });
+        invoke_buf.resize(payload_len, 0);
+
+        YCSBT {
+            put_pct: config.put_pct,
+            rng: Box::new(XorShiftRng::from_seed(seed)),
+            key_rng: Box::new(
+                ZipfDistribution::new(config.n_keys, config.skew)
+                    .expect("Couldn't create key RNG."),
+            ),
+            tenant_rng: Box::new(
+                ZipfDistribution::new(config.num_tenants as usize, config.tenant_skew)
+                    .expect("Couldn't create tenant RNG."),
+            ),
+            key_buf: key_buf,
+            value_buf: value_buf,
+            invoke_buf: invoke_buf,
+        }
     }
 }
 
 impl SendRecv for YCSBT {
-    fn get_invoke_request(&self) -> Option<Vec<u8>> {
-        let mut req = Vec::with_capacity(8);
-        req.extend_from_slice("pushback".as_bytes());
-        Some(req)
+    fn get_invoke_request(&mut self) -> (u32, &[u8]) {
+        let t = self.tenant_rng.sample(&mut self.rng) as u32;
+        (t, self.invoke_buf.as_slice())
     }
 
-    fn get_get_request(&self) -> (u32, Vec<u8>) {
-        (0, Vec::new())
+    fn get_get_request(&mut self) -> (u32, &[u8]) {
+        let t = self.tenant_rng.sample(&mut self.rng) as u32;
+
+        let k = self.key_rng.sample(&mut self.rng) as u32;
+        let k: [u8; 4] = unsafe { transmute(k.to_le()) };
+        self.key_buf[0..mem::size_of::<u32>()].copy_from_slice(&k);
+
+        (t, self.key_buf.as_slice())
+    }
+
+    fn get_put_request(&mut self) -> (u32, &[u8], &[u8]) {
+        // Sample a tenant.
+        let t = self.tenant_rng.sample(&mut self.rng) as u32;
+
+        let k = self.key_rng.sample(&mut self.rng) as u32;
+        let k: [u8; 4] = unsafe { transmute(k.to_le()) };
+        self.key_buf[0..mem::size_of::<u32>()].copy_from_slice(&k);
+        self.value_buf[0..mem::size_of::<u32>()].copy_from_slice(&k);
+
+        (t, self.key_buf.as_slice(), self.value_buf.as_slice())
+    }
+
+    fn next_optype(&mut self) -> OpCode {
+        let is_get = (self.rng.gen::<u32>() % 100) >= self.put_pct as u32;
+        if is_get == true {
+            OpCode::SandstormGetRpc
+        } else {
+            OpCode::SandstormPutRpc
+        }
     }
 }
